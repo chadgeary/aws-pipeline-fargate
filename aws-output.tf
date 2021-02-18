20dbf669e28b8afc1974394f34677698eeca135f
chadgeary <chad.geary@gmail.com>
2021-02-18 06:53:35 -0500

    output for lb, cluster, pipeline

--- aws-output.tf
+++ aws-output.tf
@@ -1,5 +1,7 @@
 output "aws-output" {
-  value = <<OUTPUT
-Load Balancer: ${aws_lb.aws-lb.dns_name}
+  value                   = <<OUTPUT
+Code Pipeline: https://console.aws.amazon.com/codesuite/codepipeline/pipelines/${var.aws_prefix}-codepipe-${random_string.aws-suffix.result}/view?region=${var.aws_region}
+Cluster Service: https://console.aws.amazon.com/ecs/home?region=${var.aws_region}#/clusters/${var.aws_prefix}-ecscluster-${random_string.aws-suffix.result}/services/${var.aws_prefix}-ecsservice-${random_string.aws-suffix.result}/tasks
+Load Balancer Target Group: https://console.aws.amazon.com/ec2/v2/home?region=${var.aws_region}#TargetGroup:targetGroupArn=${aws_lb_target_group.aws-lbtg.arn}
 OUTPUT
 }
